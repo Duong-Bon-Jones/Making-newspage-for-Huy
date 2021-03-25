da965610ea8abc925a5ad70e76f79cedcac5801f
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-03-25 23:32:52 +0700

    Define interactive sidebar.

--- js/main.js
+++ js/main.js
@@ -28,3 +28,16 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+// Nav
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
