a39e33476a34e6412cd507a327fc4df973a23a34
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-03-26 23:45:34 +0700

    Fix sidebar and finalize draft.

--- js/main.js
+++ js/main.js
@@ -31,13 +31,24 @@ document.getElementById("defaultOpen").click();
 
 // Nav
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// ! Sidenav
 function openNav() {
+    // if (window.innerWidth >= 1024) {
+    //     document.getElementById("mySidenav").style.width = "250px";
+    //     document.getElementById("main").style.marginLeft = "250px";
+    // }
+    /* Set the width of the side navigation to 250px */
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+
+    /* Set the width of the side navigation to 0 */
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
+    // if (window.innerWidth >= 1024) {
+    //     document.getElementById("mySidenav").style.width = "0";
+    //     document.getElementById("main").style.marginLeft = "0";
+    // }
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    // document.getElementById("main").style.marginLeft = "0";
 }
