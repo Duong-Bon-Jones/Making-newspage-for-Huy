5e37deb3fc49aaf283f90df080fb58ee65eecfb3
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-04-06 23:48:43 +0700

    Fix datalist alignment.

--- js/main.js
+++ js/main.js
@@ -68,6 +68,21 @@ function openCity(evt, cityName) {
     // tablinks[0].click();
 }
 
+// input.onfocus = function () {
+//     datalist.style.display = "block";
+// };
+
+// for (let option of datalist.options) {
+//     option.onclick = function () {
+//         input.value = this.value;
+//         datalist.style.display = "none";
+//     };
+// }
+
+// datalist.style.width = input.offsetWidth + "px";
+// datalist.style.left = input.offsetLeft + "px";
+// datalist.style.top = input.offsetTop + input.offsetHeight + "px";
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 // acc[0].click();
